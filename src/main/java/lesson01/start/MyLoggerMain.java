2db2ae32871be91ad176420b4e68d9713876766e
twocowsong <twocowsong@naver.com>
2025-07-03 21:25:51 +0900

    MyLogger 만들기

---
+++ src/main/java/lesson01/start/MyLoggerMain.java
@@ -0,0 +1,13 @@
+package lesson01.start;
+
+import static lesson01.utils.MyLogger.log;
+
+public class MyLoggerMain {
+
+    public static void main(String[] args) {
+        log("Hello");
+        log("123");
+
+    }
+
+}
